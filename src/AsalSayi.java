d0069ed5c6c0fa144beda5e47dabe6549161d102
egeyalcin <yalcinegehan@gmail.com>
2022-11-03 14:57:56 +0300

    asal sayi

---
+++ src/AsalSayi.java
@@ -0,0 +1,16 @@
+public class AsalSayi {
+    public static void main(String[] args) {
+        for(int i=2; i<=100 ; i++){
+            boolean isPrime = true;
+            for(int n=2; n<i ; n++){
+                if(i%n==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
+}
